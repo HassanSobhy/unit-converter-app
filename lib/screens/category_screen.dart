1a73a5142279c52538bb5eddcb73465509619b31
Hassan Sobhy <hassansobhy60@gmail.com>
2021-04-07 14:15:17 +0200

    Create CategoryScreen and set it as Home Screen

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,20 +1,59 @@
 import 'package:flutter/material.dart';
 import '../widgets/category.dart';
 
+final _backgroundColor = Colors.deepOrange[100];
+
 class CategoryScreen extends StatefulWidget {
   @override
   _CategoryScreenState createState() => _CategoryScreenState();
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
+  static const _categoryNames = <String>[
+    'Length',
+    'Area',
+    'Volume',
+    'Mass',
+    'Time',
+    'Digital Storage',
+    'Energy',
+    'Currency',
+  ];
+
   @override
   Widget build(BuildContext context) {
+    final List<Category> categories = [];
+
+    for (int i = 0; i < _categoryNames.length; i++) {
+      categories.add(Category(
+        name: _categoryNames[i],
+        icon: Icons.cake,
+      ));
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Unit Converter"),
-      ),
-      body: Center(
-        child: Category(),
+      backgroundColor: _backgroundColor,
+      appBar: _buildAppBar(),
+      body: _buildCategoryWidgets(categories),
+    );
+  }
+
+  ListView _buildCategoryWidgets(List<Category> categories) {
+    return ListView.builder(
+        itemCount: categories.length,
+        itemBuilder: (context, index) {
+          return categories[index];
+        });
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: _backgroundColor,
+      elevation: 0,
+      centerTitle: true,
+      title: Text(
+        "Unit Converter",
+        style: TextStyle(color: Colors.black, fontSize: 24),
       ),
     );
   }
--- lib/widgets/category.dart
+++ lib/widgets/category.dart
@@ -1,41 +1,51 @@
 import 'package:flutter/material.dart';
 
-final double _rowHeight = 100;
+final double _rowHeight = 75;
 final BorderRadius _borderRadius = BorderRadius.circular(_rowHeight / 2);
 
 class Category extends StatelessWidget {
+  final String name;
+  final IconData icon;
+
+  const Category({this.name, this.icon});
+
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Colors.transparent,
-      child: Container(
-        height: _rowHeight,
-        child: InkWell(
-          borderRadius: _borderRadius,
-          highlightColor: Colors.deepOrange[200],
-          splashColor: Colors.deepOrange[200],
-          onTap: () {
-            print("I was Tapped");
-          },
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(8),
-                  child: Icon(
-                    Icons.cake,
-                    size: 30,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8,vertical: 4),
+      child: Material(
+        elevation: 1,
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.deepOrange.shade50,
+        child: Container(
+          height: _rowHeight,
+          child: InkWell(
+            borderRadius: _borderRadius,
+            highlightColor: Colors.deepOrange[200],
+            splashColor: Colors.deepOrange[200],
+            onTap: () {
+              print("I was Tapped");
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Icon(
+                      icon,
+                      size: 30,
+                    ),
                   ),
-                ),
-                Center(
-                    child: Text(
-                  "Cake",
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ))
-              ],
+                  Center(
+                      child: Text(
+                    name,
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ))
+                ],
+              ),
             ),
           ),
         ),
